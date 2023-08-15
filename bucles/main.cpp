d39656dd8468e7bcf178211e25fd586d697ff55b
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-08-15 07:42:25 -0500

    Initial Commit

---
+++ bucles/main.cpp
@@ -0,0 +1,24 @@
+/*
+    Programa: Bucles - Semana #3
+    ID Autor: T00070257
+    Nombre Autor: Álvaro José Álvarez Puello
+    Fecha: 15-08-23
+ */
+
+//  Diferencia entrev for, while y do-while.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//    ciclo for
+//    solo si ya se conocen
+//    las iteraciones
+    
+    int n = 4;
+    for (int i = 0; i < n; i++) {
+        cout << "El valor de i es: " << i << endl;
+    }
+    
+    return 0;
+}
